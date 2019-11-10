1636a471725ce39aafbf3bc0a1829972ada537f8
kernel503 <juanko682@gmail.com>
2019-11-10 11:23:12 -0600

    cambio carpeta

--- tablas/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TablasConfig(AppConfig):
-    name = 'tablas'
--- tpisonrisa/settings.py
+++ tpisonrisa/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tpisonrisa.Apps.administracion',
     'tpisonrisa.Apps.tablas',
+    'tpisonrisa.Apps.voluntario',
 ]
 
 MIDDLEWARE = [
